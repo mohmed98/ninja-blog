5f72173e3e157fb50193ab5f946ffbac48d1fab9
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2021-01-13 03:21:53 +0200

    19th video

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setblogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setblogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          console.log(res);
+          if (!res.ok) {
+            throw Error("faild");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setblogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, []);
 
   return (
     <div>
-      <BlogList blogs={blogs} title="All blogs!" />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="mario's blogs!"
-      />
+      {error && <div>{error}</div>}
+
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
   );
 };
