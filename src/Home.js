0f90180a1faec5288b695d8dcddae8577858ba7c
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2021-01-13 04:28:24 +0200

    20th video

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,11 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setblogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          console.log(res);
-          if (!res.ok) {
-            throw Error("faild");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setblogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          console.log(err);
-          setError(err.message);
-          setIsPending(false);
-        });
-    }, 1000);
-  }, []);
+  const { data: blogs, isPending, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
 
   return (
     <div>
